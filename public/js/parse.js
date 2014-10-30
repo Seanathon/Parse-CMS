e12dfe210cf612d1f45d4d7ebfc3bf7b120386cc
seanathon <seanyalda@gmail.com>
2014-10-29 21:24:58 -0400

    added blog class to js

--- public/js/parse.js
+++ public/js/parse.js
@@ -5,10 +5,24 @@ $(function() {
     // Replace this line with the one on your Quickstart Guide Page
 	Parse.initialize("mr2tH58Z8CfHb30VfInRDfGY49FBqLoDNKLuQhVf", "uAdSpnycbkvZkJxNIV1Ga0Yoi3abWxrJUES8Jv3X");
  
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
-    });
- 
+ 	// Extending a JavaScript class from the Blog class on Parse.com
+	var Blog = Parse.Object.extend("Blog");
+
+	// List of blog objects in Blog class
+	var Blogs = Parse.Collection.extend({
+    	model: Blog
+	});
+
+	// Creating new instance of Blogs collection
+	var blogs = new Blogs();
+
+	// Festching and logging Blog table into console with no specifications
+	blogs.fetch({
+    success: function(blogs) {
+        console.log(blogs);
+    },
+    error: function(blogs, error) {
+        console.log(error);
+    }
+    
 });
