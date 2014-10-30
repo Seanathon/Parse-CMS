c9f4dcbdf4cfd90a094d16397f14936be7a651e2
seanathon <seanyalda@gmail.com>
2014-10-29 22:31:09 -0400

    added data, view class and view instance

--- public/js/parse.js
+++ public/js/parse.js
@@ -19,10 +19,28 @@ $(function() {
 	// Festching and logging Blog table into console with no specifications
 	blogs.fetch({
 	    success: function(blogs) {
-	        console.log(blogs);
+	    	// Upon fetch success callback, create a new instance of BlogsView
+	    	// render that new instance and put it in the main-container on the page
+	        var blogsView = new BlogsView({ collection: blogs });
+		    blogsView.render();
+		    $('.main-container').html(blogsView.el);
 	    },
 	    error: function(blogs, error) {
 	        console.log(error);
+	    }
+	});
+
+	// render blogs on page using handlebars blog template
+	var BlogsView =  Parse.View.extend({
+		// extend the Parse.View and use the template variable key
+		// to use Handlebars to compile our blogs template to HTML
+	    template: Handlebars.compile($('#blogs-tpl').html()),
+	    // then use the render function key to get the data in
+	    // the collection variable to convert this.collection into JSON
+	    // and render it with the template while assigning it to this.$el
+	    render: function(){ 
+	        var collection = { blog: this.collection.toJSON() };
+	        this.$el.html(this.template(collection));
 	    }
 	});
 
